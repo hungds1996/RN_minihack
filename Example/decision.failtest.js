2f4e64fbfc29582ae59c9a0b3ce982163ef5a84e
HungDS <hungds1996@gmail.com>
2018-05-04 20:53:23 +0700

    2nd

--- Example/decision.failtest.js
+++ Example/decision.failtest.js
@@ -1,6 +1,6 @@
 // return false to cheat, true to cooperate
 const decide = (history = []) => {
-  return history.length == 0 ? false : history[history.length - 1]['theirs']
+  return history.length == 0 ? false : history[history.length - 1]['theirs'] == true ? true : false
 }
 
 module.exports = decide;
--- Example/decision.js
+++ Example/decision.js
@@ -4,7 +4,14 @@
 //   { yours: true, theirs: false },
 // ]
 const decide = (history = []) => {
-  return history.length == 0 ? false : history[history.length - 1]['theirs'] == true ? true : false
-}
+  const theirDecisions = history.length > 0 ? Array.from({length : history.length}, (n, i) => history[i]["theirs"]) : [];
 
+  function isTrue(decision) {
+    return decision == true;
+  }
+
+  return history.length == 0 ? false : theirDecisions.slice(theirDecisions.length - 2, theirDecisions.length).every(isTrue) ? false :
+    history[history.length - 1]['theirs'] == true ? true : false
+  // return history.length == 0 ? false : history[history.length - 1]['theirs'] == true ? true : false;
+}
 module.exports = decide;
