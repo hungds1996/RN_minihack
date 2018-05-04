5fde89b4df31335fe877517cf97c23c1d5ef82eb
HungDS <hungds1996@gmail.com>
2018-05-04 21:12:13 +0700

    added another player

--- Example/decision.js
+++ Example/decision.js
@@ -6,11 +6,7 @@
 const decide = (history = []) => {
   const theirDecisions = history.length > 0 ? Array.from({length : history.length}, (n, i) => history[i]["theirs"]) : [];
 
-  function isTrue(decision) {
-    return decision == true;
-  }
-
-  return history.length == 0 ? false : theirDecisions.slice(theirDecisions.length - 2, theirDecisions.length).every(isTrue) ? false :
+  return history.length == 0 ? true : theirDecisions.slice(theirDecisions.length - 2, theirDecisions.length).every(decision => decision == true) ? false :
     history[history.length - 1]['theirs'] == true ? true : false
   // return history.length == 0 ? false : history[history.length - 1]['theirs'] == true ? true : false;
 }
